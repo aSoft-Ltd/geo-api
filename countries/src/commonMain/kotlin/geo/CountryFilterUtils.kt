1e695eb51e5bbe63cb18b0464ebf5909438db3ab
andylamax <andylamax@programmer.net>
2024-01-15 15:26:41 +0300

    Added missing methods in ReportingApiPione

--- countries/src/commonMain/kotlin/geo/CountryFilterUtils.kt
+++ countries/src/commonMain/kotlin/geo/CountryFilterUtils.kt
@@ -1,8 +1,6 @@
-@file:Suppress("NOTHING_TO_INLINE")
-
 package geo
 
-inline fun Country.matches(key: String): Boolean = label.contains(key, ignoreCase = true)
+fun Country.matches(key: String): Boolean = label.contains(key, ignoreCase = true)
         || code.contains(key, ignoreCase = true)
         || currency.name.contains(key, ignoreCase = true)
         || dialingCode.contains(key.replace("+", ""))
